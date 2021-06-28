9a48116ca22b2048a87fe5ae9abaca9153005497
Ankit Dongre <ankitdongre96@gmail.com>
2021-06-28 19:35:11 +0100

    Added JWT Authentication

--- src/main/java/com/example/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservices/controllers/HelloWorldController.java
@@ -1,24 +1,21 @@
 package com.example.restfulwebservices.controllers;
 
-import com.example.restfulwebservices.beans.HelloWorldBean;
+import com.example.restfulwebservices.beans.AuthenticationBean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class HelloWorldController {
 
 
-    @RequestMapping(method = RequestMethod.GET,path = "/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
-        return new HelloWorldBean("Hello World");
-    }
-
-    @RequestMapping(method = RequestMethod.GET,path = "/hello-world-bean/{name}")
-    public HelloWorldBean helloWorldBeanWithName(@PathVariable String name){
-
-        return new HelloWorldBean("Hello " +name);
+    @RequestMapping(method = RequestMethod.GET,path = "/basicauth")
+    public AuthenticationBean authenticate(){
+        return new AuthenticationBean("You are authenticated");
     }
 
     @RequestMapping(value="/", method = RequestMethod.OPTIONS)
